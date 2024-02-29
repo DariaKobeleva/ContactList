d2c72c8042657436dfc598c0c1998e5cdb29d003
KDaria <dkdva@mail.ru>
2024-02-29 18:24:25 +0100

    Refactoring

--- ContactList/ViewControllers/ContactViewController.swift
+++ ContactList/ViewControllers/ContactViewController.swift
@@ -10,7 +10,22 @@ import UIKit
 final class ContactViewController: UITableViewController {
     
     // MARK: - Properties
-    private var contactList = Person.getContactList()
+    var contactList = Person.getContactList()
+    
+    // MARK: - UITableViewDataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        contactList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
+        let person = contactList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = person.fullName
+        cell.contentConfiguration = content
+        return cell
+    }
     
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -20,21 +35,5 @@ final class ContactViewController: UITableViewController {
     }
 }
 
-// MARK: - UITableViewDataSource
-extension ContactViewController {
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        contactList.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
-        let person = contactList[indexPath.row]
-        var content = cell.defaultContentConfiguration()
-        content.text = person.fullName
-        
-        cell.contentConfiguration = content
-        return cell
-    }
-}
 
 
